fb9c71bccc37c784a8095f8c59b95ff09c8cd387
liboz <ringil.sword@gmail.com>
2018-01-08 18:27:38 -0500

    reading lines of the matrix

--- PillarKata/WordSearch.cs
+++ PillarKata/WordSearch.cs
@@ -8,10 +8,31 @@ namespace PillarKata
 {
     public class WordSearch
     {
-        public static string[] Parse(string testString)
+        public static string[] ParseFirstLine(string wordString)
         {
-            var result = testString.Split(',');
+            var result = wordString.Split(',');
             return result;
+        }
+
+        public static string[,] ParseMatrix(string[] data)
+        {
+            var firstLine = ParseMatrixLine(data[0]);
+            var result = new string[firstLine.Length, firstLine.Length];
+            return null;
+        }
+
+        public static string[] ParseMatrixLine(string data)
+        {
+            var result = data.Split(',');
+            return result;
+        }
+
+        public static void InsertLineIntoMatrix(string[,] matrix, string[] data, int v)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                matrix[v, i] = data[i];
+            }
         }
     }
 }
--- PillarKataTests/UnitTest1.cs
+++ PillarKataTests/UnitTest1.cs
@@ -1,16 +1,34 @@
 ﻿using PillarKata;
 using Xunit;
 using System;
+using System.Text.RegularExpressions;
 
 namespace PillarKataTests
 {
     public class UnitTest1
     {
+        public static readonly string testData =
+@"U,M,K,H,U,L,K,I,N,V,J,O,C,W,E
+L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G
+H,S,U,P,J,P,R,J,D,H,S,B,X,T,G
+B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E
+A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D
+S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F
+B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z
+O,K,R,I,K,A,M,M,R,M,F,B,A,P,P
+N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S
+E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K
+S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D
+T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K
+O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H
+W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S
+K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B";
+
         [Fact]
         public void ReadFirstLineHappyPath()
         {
             var testString = "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA";
-            var result = WordSearch.Parse(testString);
+            var result = WordSearch.ParseFirstLine(testString);
             Assert.Equal(7, result.Length);
             Assert.Contains("BONES", result);
             Assert.Contains("KHAN", result);
@@ -24,13 +42,35 @@ namespace PillarKataTests
         [Fact]
         public void ReadFirstLineGarbageData()
         {
-            var result = WordSearch.Parse("");
+            var result = WordSearch.ParseFirstLine("");
             Assert.Single(result);
 
-            result = WordSearch.Parse("BONES;KHAN;KIRK;SCOTTY;SPOCK;SULU;UHURA");
+            result = WordSearch.ParseFirstLine("BONES;KHAN;KIRK;SCOTTY;SPOCK;SULU;UHURA");
             Assert.Single(result);
 
-            Assert.Throws<NullReferenceException>(() => WordSearch.Parse(null));
+            Assert.Throws<NullReferenceException>(() => WordSearch.ParseFirstLine(null));
+        }
+
+        [Fact]
+        public void ReadMatrix()
+        {
+            var arr = Regex.Split(testData, "\r\n|\r|\n");
+            var result = WordSearch.ParseMatrix(arr);
+            Assert.Equal(15, result.GetLength(0));
+            Assert.Equal(15, result.GetLength(1));
+
+        }
+
+        [Fact]
+        public void InsertLineIntoMatrixHappyPath()
+        {
+            var matrix = new string[2, 2];
+            var data = new string[] { "a", "b" };
+            WordSearch.InsertLineIntoMatrix(matrix, data, 0);
+            Assert.Equal("a", matrix[0, 0]);
+            Assert.Equal("b", matrix[0, 1]);
+            Assert.Null(matrix[1, 0]);
+            Assert.Null(matrix[1, 1]);
         }
     }
 }
